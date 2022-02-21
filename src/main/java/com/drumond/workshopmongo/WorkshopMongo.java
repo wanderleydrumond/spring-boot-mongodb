5c89f771fd26663d1ec17fe9009cef9e4590ed54
Wanderley Drumond <wanderley.drumond@gmail.com>
2022-02-21 12:35:52 +0000

    Projeto criado

---
+++ src/main/java/com/drumond/workshopmongo/WorkshopMongo.java
@@ -0,0 +1,11 @@
+package com.drumond.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopMongo {
+    public static void main(String[] args) {
+        SpringApplication.run(WorkshopMongo.class, args);
+    }
+}
